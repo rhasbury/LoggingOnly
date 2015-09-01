c4b0c67e251ebd2531986e3b9db72a303a9c3723
rhasbury <rhasbury@gmail.com>
2015-09-01 12:10:17 -0400

    Added date selection to http plotting page. Fixed logging problem in python main loop.

--- LoggingOnlyScript.py
+++ LoggingOnlyScript.py
@@ -37,6 +37,7 @@ gpsp = None #
 tmp = None
 lcd = None
 
+
 #def setup():
 
  # start it up
@@ -60,7 +61,7 @@ def signal_quitting(signal, frame):
     gpsp.join() # wait for the thread to finish what it's doing        
     logging.info("Received Sigint, quitting")
     logging.debug('You pressed Ctrl+C!')    
-    sys.exit(0)
+    sys.exit(0)    
 
 
 def logTemplineDB(location, temp):    
@@ -69,10 +70,10 @@ def logTemplineDB(location, temp):
         with connection.cursor() as cursor:
             cursor.execute ("INSERT INTO tempdat values(NOW(), %s, %s)", (location, temp))
         connection.commit()
-        logging.debug("logTempLineDB() Rows logged: %s" % cursor.rowcount)
+        logging.info("logTempLineDB() Rows logged: %s" % cursor.rowcount)
         connection.close()
     except:
-        logging.debug("logTempLineDB Temperature Logging exception Error %s" % sys.exc_info()[0])
+        logging.debug("logTempLineDB Temperature Logging exception Error ", exc_info=True)
 
 
 
@@ -90,15 +91,18 @@ def UpdateTemps():
     
     try:
         EngineTemp = thermocouple.get()
-        logTemplineDB("engine", EngineTemp)        
+        logTemplineDB("engine", EngineTemp)     
+    except KeyboardInterrupt:                        
+        raise   
     except MAX6675Error as e:
         EngineTemp = "Error: "+ e.value
-        logging.debug("UpdateTemps() Excepted getting enginetemp: %s" % EngineTemp)
+        logging.debug("UpdateTemps() Excepted getting enginetemp: ", exc_info=True)
     
-    lcdstring = " %sC" % (EngineTemp)
-    lcd.clear
-    lcd.setPosition(1, 0) 
-    lcd.writeString(lcdstring)
+    if (lcd != None):
+        lcdstring = " %sC" % (EngineTemp)
+        lcd.clear
+        lcd.setPosition(1, 0) 
+        lcd.writeString(lcdstring)
 
 
 def LogGPSPoint():
@@ -107,51 +111,53 @@ def LogGPSPoint():
     global AmbientTemp
     global tmp
     global lcd
-    
+ 
+    resp = ""
+   
     try:
         con = pymysql.connect(host='localhost', user='monitor', passwd='password', db='temps', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
         cur = con.cursor()
+    except KeyboardInterrupt:                        
+        raise
     except:
-        logging.debug("LogGPSPoint: Error opening MySQL connection %s" % sys.exc_info()[0])
+        logging.debug("LogGPSPoint: Error opening MySQL connection ", exc_info=True)
         sys.exit(1)
 
-    resp = ""
-    
-    try:
-        lcdstring = "%sm   %s" % (gpsd.fix.altitude, gpsd.fix.track)
-        lcd.setPosition(2, 0) 
-        lcd.writeString(lcdstring)
-        gtime = dateutil.parser.parse(gpsd.utc)            
-        lcdstring = "%s" % (gtime.strftime('%I:%M'))
-        lcd.setPosition(1, 8)
-        lcd.writeString(lcdstring)
-        
-    except:
-        logging.debug("LogGPSPoint() failed to write to LCD %s" % sys.exc_info()[0])  
-   
+
     try:    
-        if(gpsd.fix.mode == 3 or gpsd.fix.mode == 3):        
+        if(gpsd.fix.mode == 3):        
             #print ('time utc    ' , gpsd.utc)
             #print ('time utc    ' , gpsd.fix.time)
 
             sql = "insert into gps(n_lat, w_long, date_time, speed, altitude, mode, track, climb, enginetemp, ambienttemp) values(%s, %s, NOW(), %s, %s, %s, %s, %s, %s, %s)" % (gpsd.fix.latitude, gpsd.fix.longitude, gpsd.fix.speed, gpsd.fix.altitude, gpsd.fix.mode, gpsd.fix.track, gpsd.fix.climb, EngineTemp, AmbientTemp)
             sql = sql.replace("nan", "-9999")
-            #print (sql)
             cur.execute(sql)
-            logging.info("Rows inserted: %s" % cur.rowcount)
             con.commit()
-        #else:
-            #print ("no fix")      
-
+            #logging.info("Rows inserted: %s" % cur.rowcount)
+            #logging.info("SQL String: %s" % sql)
+    except KeyboardInterrupt:                        
+        raise
     except:
         #print (sys.exc_info()[0])
-        logging.debug("LogGPSPoint() excepted trying to log GPS data %s" % sys.exc_info()[0])
+        logging.debug("LogGPSPoint() excepted trying to log GPS data ", exc_info=True)
 
     finally:
         if con:
             con.close()
 
-
+    try:
+        if(lcd != None and gpsd.fix.mode == 3):            
+            lcdstring = "%sm   %s" % (gpsd.fix.altitude, gpsd.fix.track)
+            lcd.setPosition(2, 0) 
+            lcd.writeString(lcdstring)
+            gtime = dateutil.parser.parse(gpsd.utc)            
+            lcdstring = "%s" % (gtime.strftime('%I:%M'))
+            lcd.setPosition(1, 8)
+            lcd.writeString(lcdstring)        
+    except KeyboardInterrupt:                        
+        raise
+    except:
+        logging.debug("LogGPSPoint() failed to write to LCD ", exc_info=True) 
 
 
 
@@ -159,30 +165,51 @@ def LogGPSPoint():
 class GpsPoller(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        global gpsd #bring it in scope
-        try:
-            gpsd = GPS(mode=WATCH_ENABLE) #starting the stream of info
-            self.current_value = None
-            self.running = True #setting the thread running to true
-        except:
-            logging.debug("GPSPoller() excepted connecting to GPSD %s" % sys.exc_info()[0])
-            exit(0)
+        self.current_value = None
+        self.running = True #setting the thread running to true
+
  
     def run(self):
         global gpsd
         global gpsp
         global EngineTemp
         global AmbientTemp
+        
+        gps_connected = False
+        
         while gpsp.running:
+            
+            try:
+                gpsd = GPS(mode=WATCH_ENABLE) #starting the stream of info
+                gps_connected = True
+            except KeyboardInterrupt:                        
+                raise
+            except:
+                logging.debug("GPSPoller() excepted connecting to GPSD ", exc_info=True)
+                gps_connected = False
+
+            
             UpdateTemps()
-            if(gpsd.waiting(3000)):
-                try:
-                    gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
-                    LogGPSPoint()
-                except:
-                    logging.debug("run() excepted running gpsd.next() %s" % sys.exc_info()[0])
+            while(gps_connected == True):
+                if(gpsd.waiting(3000)):                
+                    try:
+                        gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+                        LogGPSPoint()
+                    except JsonError:
+                        logging.debug("run() -> gpsd.next() threw JsonError", exc_info=True)
+                        gps_connected = False                
+                    except ValueError:
+                        logging.debug("run() -> gpsd.next() threw ValueError", exc_info=True)
+                        gps_connected = False
+                    except StopIteration:                    
+                        logging.debug("run() -> gpsd.next() threw stopiteration", exc_info=True)
+                        gps_connected = False
+                    except KeyboardInterrupt:                        
+                        raise
+                    
+                time.sleep(0.5)
                 
-            time.sleep(3)
+            time.sleep(5)
              
  
  
@@ -190,15 +217,19 @@ if __name__ == "__main__":
 #    global gpsp
 #    global tmp
 #    tmp = BMP085.BMP085()
+    logging.getLogger().setLevel(logging.DEBUG)
     logging.info("Logging started")
     signal.signal(signal.SIGINT, signal_quitting)
     
     # Configuration parameters
     # I2C Address, Port, Enable pin, RW pin, RS pin, Data 4 pin, Data 5 pin, Data 6 pin, Data 7 pin, Backlight pin (optional)
-    lcd = i2c_lcd.i2c_lcd(0x27, 1, 2, 1, 0, 4, 5, 6, 7, 3)
-    lcd.backLightOn()
- 
-    
+    try:
+        lcd = i2c_lcd.i2c_lcd(0x27, 1, 2, 1, 0, 4, 5, 6, 7, 3)
+        lcd.backLightOn()
+    except IOError:
+        logging.debug("LCD IO Error", exc_info=True)
+        lcd = None
+        
     gpsp = GpsPoller()
     gpsp.start()
 
